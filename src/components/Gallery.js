8892cd3762576b633b4f93769e066315ebcdc6a2
Fattah <fattah.afr2@gmail.com>
2026-05-17 23:51:25 +0700

    fix gallery js lagi

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -28,9 +28,23 @@ function Gallery() {
   };
 
   useEffect(() => {
+    const fetchCharacterData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.jikan.moe/v4/characters/${id}`,
+        );
+        const data = await response.json();
+        setCharacterData(data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error:", error);
+        setIsLoading(false);
+      }
+    };
+
     getAnimePictures(id);
     fetchCharacterData();
-  }, [id, getAnimePictures, fetchCharacterData]);
+  }, [id, getAnimePictures]);
 
   if (isLoading) {
     return <LoadingSpinner>Loading...</LoadingSpinner>;
